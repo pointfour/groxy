1fceb8629c32261cd0f3f614136379e8f5513a4c
Aleksei Seletskiy <aseletskiy@cloudlinux.com>
2019-07-31 20:10:07 +0100

    now uses duck duck go bang parser

--- main.js
+++ main.js
@@ -11,32 +11,10 @@ function parseUrl(url) {
   return query.join('')
 }
 
-const bangs = require('./bangs')
-function parseBang(query) {
-  console.log(query)
-  let q = query.replace('!', '')
-  console.log(q)
-  q = q.split('+')
-  console.log(q)
-  let name = q[0]
-  let newquery;
-  if (q.length == 2)
-    newquery = q.slice(1)
-  else
-    newquery = q.slice(1, q.length - 1).join('+')
-  console.log(name, newquery)
-  return bangs[name] + newquery
-}
-
-
 app.get('/search', (req, res) => {
   let query = parseUrl(req.originalUrl)
   if (query[0] == "!") {
-    if (parseBang(query)) {
-      res.redirect(parseBang(query))
-    } else {
-      res.redirect(`https://www.google.com/search?q=${query}`)
-    }
+    res.redirect(`https://duckduckgo.com/?q=${query}`)
   } else {
     res.redirect(`https://www.google.com/search?q=${query}`)
   }
